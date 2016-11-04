24abd9d2c98858519d0ae9f15d6476507196a09d
Syl26 <syl.zieba26@gmail.com>
2016-11-04 00:13:47 -0400

    new section and typewriter commit

--- js/init.js
+++ js/init.js
@@ -4,6 +4,25 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
       
+      var str = "<p>Hi, my name is Sylwia.</p>",
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 80);
+    }());
+      
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
